31d7a7bbcacc9568012130965db5a5642b7a2419
aleksejjj <aleserkan@gmail.com>
2024-09-16 16:35:05 +0400

    Товары.

--- search.php
+++ search.php
@@ -1,12 +1,29 @@
 <?php
 	include 'mnn.php';
 
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+            header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        }
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        }
+
+        exit(0);
+    }
 
 	header('Content-Type: application/json; charset=UTF-8');
 	$link = firstconnect ();
-	//[$session_id, $username] = enterregistration ();	
-	
+	//[$session_id, $username] = enterregistration ();
+
 	$search = mb_substr($_GET['search'], 0, 7);
 	$res = ExecSQL($link,"SELECT name, CONCAT('https://fitokrama.by/art_page.php?art=',art) as art FROM goods WHERE name like '%$search%' or art='$search' or barcode='$search' LIMIT 20;");
-	
+
 	exit( json_encode($res, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
