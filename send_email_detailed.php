892a62012b781b221612244bf00efae90bbe7850
xbro1305 <x.bro2010@gmail.com>
2024-09-19 16:26:35 +0500

    email letter

--- send_email_detailed.php
+++ send_email_detailed.php
@@ -17,18 +17,18 @@
 	$order_address_delivery = $order['order_address_delivery'];
 	
 	$doc = file_get_contents("./pages/for_mail_stages.html");
-	if (intval($stage)>1) $doc = str_replace('[img_1]', './logos/ok_green.png', $doc);
-						$doc = str_replace('[img_1]', './logos/not_ok_green.png', $doc);
-	if (intval($stage)>2) $doc = str_replace('[img_2]', './logos/ok_green.png', $doc);
-						$doc = str_replace('[img_2]', './logos/not_ok_green.png', $doc);
-	if (intval($stage)>3) $doc = str_replace('[img_3]', './logos/ok_green.png', $doc);
-						$doc = str_replace('[img_3]', './logos/not_ok_green.png', $doc);
-	if (intval($stage)>4) $doc = str_replace('[img_4]', './logos/ok_green.png', $doc);
-						$doc = str_replace('[img_4]', './logos/not_ok_green.png', $doc);
-	if (intval($stage)>5) $doc = str_replace('[img_5]', './logos/ok_green.png', $doc);
-						$doc = str_replace('[img_5]', './logos/not_ok_green.png', $doc);
-	if (intval($stage)>6) $doc = str_replace('[img_6]', './logos/ok_green.png', $doc);
-						$doc = str_replace('[img_6]', './logos/not_ok_green.png', $doc);
+	if (intval($stage)>1) $doc = str_replace('[stage_1]', 'stage succeeded', $doc);
+						$doc = str_replace('[stage_1]', 'stage', $doc);
+	if (intval($stage)>2) $doc = str_replace('[stage_2]', 'stage succeeded', $doc);
+						$doc = str_replace('[stage_2]', 'stage', $doc);
+	if (intval($stage)>3) $doc = str_replace('[stage_3]', 'stage succeeded', $doc);
+						$doc = str_replace('[stage_3]', 'stage', $doc);
+	if (intval($stage)>4) $doc = str_replace('[stage_4]', 'stage succeeded', $doc);
+						$doc = str_replace('[stage_4]', 'stage', $doc);
+	if (intval($stage)>5) $doc = str_replace('[stage_5]', 'stage succeeded', $doc);
+						$doc = str_replace('[stage_5]', 'stage', $doc);
+	if (intval($stage)>6) $doc = str_replace('[stage_6]', 'stage succeeded', $doc);
+						$doc = str_replace('[stage_6]', 'stage', $doc);
 						
 	$doc = str_replace('[text_detailed]', 'Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. ', $doc);
 	$doc = str_replace('[order_number]', $order_number, $doc);
