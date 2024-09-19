cb936f5085715e7777646c65a7a5362e05c23567
Emil Kenherli <kenherli@gmail.com>
2024-09-19 11:55:48 +0200

    добавлена отправка email со статусами заказов-2

--- send_email_detailed.php
+++ send_email_detailed.php
@@ -17,18 +17,18 @@
 	$order_address_delivery = $order['order_address_delivery'];
 	
 	$doc = file_get_contents("./pages/for_mail_stages.html");
-	if (int($stage)>1) $doc = str_replace('[1_ok]', '<img src="./logos/ok_green.png" alt="" />', $doc);
-						else $doc = str_replace('[1_ok]', '<img src="./logos/not_ok_green.png" alt="" />', $doc);
-	if (int($stage)>2) $doc = str_replace('[2_ok]', '<img src="./logos/ok_green.png" alt="" />', $doc);
-						else $doc = str_replace('[2_ok]', '<img src="./logos/not_ok_green.png" alt="" />', $doc);
-	if (int($stage)>3) $doc = str_replace('[3_ok]', '<img src="./logos/ok_green.png" alt="" />', $doc);
-						else $doc = str_replace('[3_ok]', '<img src="./logos/not_ok_green.png" alt="" />', $doc);
-	if (int($stage)>4) $doc = str_replace('[4_ok]', '<img src="./logos/ok_green.png" alt="" />', $doc);
-						else $doc = str_replace('[4_ok]', '<img src="./logos/not_ok_green.png" alt="" />', $doc);
-	if (int($stage)>5) $doc = str_replace('[5_ok]', '<img src="./logos/ok_green.png" alt="" />', $doc);
-						else $doc = str_replace('[5_ok]', '<img src="./logos/not_ok_green.png" alt="" />', $doc);
-	if (int($stage)>6) $doc = str_replace('[6_ok]', '<img src="./logos/ok_green.png" alt="" />', $doc);
-						else $doc = str_replace('[6_ok]', '<img src="./logos/not_ok_green.png" alt="" />', $doc);
+	if (intval($stage)>1) $doc = str_replace('[img_1]', './logos/ok_green.png', $doc);
+						$doc = str_replace('[img_1]', './logos/not_ok_green.png', $doc);
+	if (intval($stage)>2) $doc = str_replace('[img_2]', './logos/ok_green.png', $doc);
+						$doc = str_replace('[img_2]', './logos/not_ok_green.png', $doc);
+	if (intval($stage)>3) $doc = str_replace('[img_3]', './logos/ok_green.png', $doc);
+						$doc = str_replace('[img_3]', './logos/not_ok_green.png', $doc);
+	if (intval($stage)>4) $doc = str_replace('[img_4]', './logos/ok_green.png', $doc);
+						$doc = str_replace('[img_4]', './logos/not_ok_green.png', $doc);
+	if (intval($stage)>5) $doc = str_replace('[img_5]', './logos/ok_green.png', $doc);
+						$doc = str_replace('[img_5]', './logos/not_ok_green.png', $doc);
+	if (intval($stage)>6) $doc = str_replace('[img_6]', './logos/ok_green.png', $doc);
+						$doc = str_replace('[img_6]', './logos/not_ok_green.png', $doc);
 						
 	$doc = str_replace('[text_detailed]', 'Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. Детальное описание этапов заказа. ', $doc);
 	$doc = str_replace('[order_number]', $order_number, $doc);
