555d175ad4ae818d3b2138ee2d50f026ba102e0e
aleksejjj <aleserkan@gmail.com>
2024-09-20 13:55:02 +0400

    Меню по ролям.

--- admin/components/SidebarMenu.vue
+++ admin/components/SidebarMenu.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { useAuthStore } from '~/store/auth'
+
+const { role } = useAuthStore()
+
 const items = [
   {
     title: 'Автопечать',
@@ -7,42 +11,55 @@ const items = [
       prependIcon: 'mdi-printer',
     },
   },
-  {
+]
+
+if (role) {
+  items.push({
     title: 'Все заказы',
     props: {
       to: '/',
       prependIcon: 'mdi-archive',
     },
-  },
-  {
+  })
+}
+
+if (['main', 'store'].includes(role)) {
+  items.push({
     title: 'Сборка заказа',
     props: {
       to: '/assembly_order',
       prependIcon: 'mdi-archive-arrow-down',
     },
-  },
-  {
+  })
+
+  items.push({
     title: 'Сборка на почту',
     props: {
       to: '/assembly_post',
       prependIcon: 'mdi-archive-arrow-down',
     },
-  },
-  {
+  })
+}
+
+if (['main', 'postman'].includes(role)) {
+  items.push({
     title: 'Отправка почты',
     props: {
       to: '/send_post',
       prependIcon: 'mdi-archive-arrow-up',
     },
-  },
-  {
+  })
+}
+
+if (['main', 'buyer', 'manager'].includes(role)) {
+  items.push({
     title: 'Товары',
     props: {
       to: '/products',
       prependIcon: 'mdi-archive',
     },
-  },
-]
+  })
+}
 </script>
 
 <template>
